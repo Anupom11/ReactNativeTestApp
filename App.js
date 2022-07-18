51ecbc3f2adf5b1703f09ec80db5e99e34a2fc4a
Anupom11 <anupom.chakrabarty@gmail.com>
2022-07-18 23:38:12 +0530

    update test app with user input operation

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
+
+  const [userInputVal, setUserInputVal] = useState('');
+
+  function getTextInputData(textData) {
+    setUserInputVal(textData);
+  }
+
+  function addUserInput() {
+    console.log("Val::"+userInputVal);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text style={{margin:50}}>Hello World!!!!</Text>
-      <Button title='Click Me'/>
+    <View style={styles.appContainer}>
+      <View>
+        <Text style={{margin:10}}>Hello World!!!!</Text>
+        <Button title='Click Me'/>
+      </View>
+
+      <View style={styles.inputContainer}>
+        
+        <View style={{borderColor: '#000000', borderWidth:1, marginEnd:10}}>
+          <TextInput placeholder='Your course goal!' onChangeText={getTextInputData} />
+        </View>
+
+        <Button title='Add Goal' onPress={addUserInput}/>
+      
+      </View>
+
     </View>
+
   );
 }
 
@@ -17,4 +42,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  appContainer: {
+    padding: 50,
+  },
+
+  inputContainer: {
+    flexDirection:'row',
+    padding:5,
+    justifyContent:'flex-start',
+  }
+
 });
