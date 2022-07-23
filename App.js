d643595805b8bed936cd6e48c6fef49d55eb35fe
Anupom11 <anupom.chakrabarty@gmail.com>
2022-07-23 19:21:35 +0530

    updated with list view

--- App.js
+++ App.js
@@ -4,30 +4,42 @@ import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 export default function App() {
 
   const [userInputVal, setUserInputVal] = useState('');
+  const [courseGoals, setCourseGoals]   = useState([]);
 
   function getTextInputData(textData) {
     setUserInputVal(textData);
   }
 
   function addUserInput() {
-    console.log("Val::"+userInputVal);
+    //console.log("Val::"+userInputVal);
+    setCourseGoals((currentCourseGoals)=>[...currentCourseGoals, userInputVal]);
+  }
+
+  function clickMeResponse() {
+    alert("Click me!");
   }
 
   return (
     <View style={styles.appContainer}>
       <View>
         <Text style={{margin:10}}>Hello World!!!!</Text>
-        <Button title='Click Me'/>
+        <Button title='Click Me' onPress={clickMeResponse}/>
       </View>
 
       <View style={styles.inputContainer}>
-        
         <View style={{borderColor: '#000000', borderWidth:1, marginEnd:10}}>
           <TextInput placeholder='Your course goal!' onChangeText={getTextInputData} />
         </View>
 
         <Button title='Add Goal' onPress={addUserInput}/>
-      
+      </View>
+
+      <View>
+        {
+          courseGoals.map((goal)=>
+            <Text key={goal}>{goal}</Text>
+          )
+        }
       </View>
 
     </View>
